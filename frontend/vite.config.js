14e7ae37736644847cf7684683e63b7e8cc44609
mar7yyy <144794313+martinoparisi@users.noreply.github.com>
2026-07-02 16:26:11 +0000

    fix: fix for codespaces

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173, host: true, allowedHosts: ['.trycloudflare.com'] },
+  server: { port: 5173, host: true, allowedHosts: ['.trycloudflare.com', '.app.github.dev'] },
   build: {
     rollupOptions: {
       output: {
